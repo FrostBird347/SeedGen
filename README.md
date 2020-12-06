2f129ceebfd20aa4c79380beb59275f3f2b0463d
FrostBird347 <39435218+FrostBird347@users.noreply.github.com>
2020-12-06 12:11:31 +0800

    SeedGen 1.0.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# SeedGen
+An over-engineered seed generator
+
+A seed is generated from the computer's name, os version, processor count, tick count, user name and the previous seed.
---
+++ SeedGen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SeedGen
+{
+	public static class Main
+	{
+		private static int LastSeed = 0;
+
+		public static int GenSeed()
+		{
+			int Seed = GenHash(Environment.MachineName + Environment.OSVersion.VersionString + Environment.ProcessorCount.ToString() + Environment.TickCount.ToString() + Environment.UserName + LastSeed.ToString());
+			return Seed;
+		}
+
+		private static int GenHash(string Input)
+		{
+			using (MD5 md5 = MD5.Create())
+			{
+				md5.Initialize();
+				byte[] InputBytes = Encoding.ASCII.GetBytes(Input);
+				byte[] HashBytes = md5.ComputeHash(InputBytes);
+				int HashInt = BitConverter.ToInt32(HashBytes, 0);
+				LastSeed = HashInt;
+				return HashInt;
+			}
+		}
+	}
+}
